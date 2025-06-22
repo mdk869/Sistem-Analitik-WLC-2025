f0c4b55f293cb89083e04d1624a1a88e5e1e835e
mdk869 <82664810+mdk869@users.noreply.github.com>
2025-06-22 12:56:38 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -8,16 +8,10 @@ import json
 from datetime import datetime
 import pytz
 
-# Google Sheets Setup
-scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-
-if "GOOGLE_SHEETS_CREDS" in os.environ:
-    creds_dict = json.loads(os.environ["GOOGLE_SHEETS_CREDS"])
-    creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
-else:
-    cred_path = os.path.join(os.path.dirname(__file__), "credentials.json")
-    creds = ServiceAccountCredentials.from_json_keyfile_name(cred_path, scope)
-
+# Guna credentials dari secrets Streamlit
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+credentials_dict = st.secrets["gcp_service_account"]
+creds = ServiceAccountCredentials.from_json_keyfile_dict(credentials_dict, scope)
 
 client = gspread.authorize(creds)
 sheet = client.open("peserta").worksheet("Sheet1")
