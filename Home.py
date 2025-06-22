beeac081cced942b64e6967d7e82cfd7eb12d591
mdk869 <muhammad.khalis.rahim@gmail.com>
2025-06-22 22:09:01 +0800

    update

--- Home.py
+++ Home.py
@@ -1,6 +1,9 @@
 # Home.py
 import streamlit as st
-from app.styles import paparkan_tema, papar_tajuk_utama, papar_kandungan_home, papar_header, papar_footer
+from datetime import datetime
+import pytz
+
+from app.styles import paparkan_tema, papar_footer, papar_tajuk_utama, papar_kandungan_home
 
 # === Setup Paparan ===
 st.set_page_config(
@@ -9,8 +12,23 @@ st.set_page_config(
     layout="wide"
 )
 
-# === Tema dan Kandungan ===
+# === Paparkan Tema dan Tajuk ===
 paparkan_tema()
 papar_tajuk_utama()
 papar_kandungan_home()
-papar_footer("MKR")
+
+# === Navigasi Sidebar ===
+with st.sidebar:
+    st.header("📁 Navigasi")
+
+    if st.button("🔐 Admin Panel"):
+        st.switch_page("pages/admin.py")
+
+    if st.button("📈 Dashboard"):
+        st.switch_page("pages/dashboard.py")
+
+
+# === Footer ===
+local_tz = pytz.timezone("Asia/Kuala_Lumpur")
+footer_date = datetime.now(local_tz).strftime("%d/%m/%Y")
+papar_footer("MKR", footer_date)
