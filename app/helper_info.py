288e1eacc12217327d751cff40e4ce26e721ce67
mdk869 <muhammad.khalis.rahim@gmail.com>
2025-06-26 01:29:16 +0800

    Update helper_info.py

--- app/helper_info.py
+++ app/helper_info.py
@@ -14,3 +14,16 @@ def get_motivasi_harian():
         "Makan untuk hidup, bukan hidup untuk makan."
     ]
     return random.choice(motivasi_list)
+
+def get_tips_nutrisi():
+    nutrisi_list = [
+        "🍽️ Kawal Saiz Hidangan",
+        "🥗 Utamakan Makanan Segar",
+        "🥚 Cukupkan Protein",
+        "🚫 Kurangkan:Minuman manis (teh tarik, air gas, sirap). Makanan bergoreng dan berminyak. Gula tersembunyi (biskut, kek, roti putih).Garam berlebihan (jajan, makanan segera).",
+        "💧 Minum Air Cukup: Sasaran minimum 2–3 liter sehari. Elakkan air manis sebagai pengganti.",
+        "⏰ Makan Ikut Jadual: Sarapan ringan + makan tengah hari seimbang + makan malam sederhana. ",
+        "🏃‍♂️ Sokong dengan Aktiviti Fizikal: Sasaran 150 minit aktiviti fizikal sederhana seminggu."
+       
+    ]
+    return random.choice(nutrisi_list)
