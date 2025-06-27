583880729b089761f994eff519abae1914b1c0cb
mdk869 <muhammad.khalis.rahim@gmail.com>
2025-06-27 13:30:07 +0800

    Update dashboard.py

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -180,7 +180,7 @@ with tab2:
         df_rank = df_rank.head(int(pilihan_top))
 
     st.dataframe(
-        df_rank[["Ranking", "Nama", "% Penurunan"]],
+        df_rank.columns[["Ranking", "Nama", "% Penurunan"]],
         use_container_width=True,
         hide_index=True
     )
