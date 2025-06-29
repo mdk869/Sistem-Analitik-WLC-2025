2efd2bd1bd9f7b2567955ffe06b99d4e5bc6c39d
mdk869 <muhammad.khalis.rahim@gmail.com>
2025-06-29 21:01:04 +0800

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -1,11 +1,4 @@
 # app/settings.py
-
-TARIKH_MULA = "18 Mei 2025"
-TARIKH_TIMBANG_SET = "20 Julai 2025"
-NAMA_PROGRAM = "Weight Loss Challenge 2025"
-VERSI = "v3.2.5"
-LAST_UPDATE = "2025-06-26"
-
 from datetime import datetime
 
 info_program = {
