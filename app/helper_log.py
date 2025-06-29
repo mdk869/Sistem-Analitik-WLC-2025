8a80d5f5c53c45bd231326c4832b0176dc2164dc
mdk869 <muhammad.khalis.rahim@gmail.com>
2025-06-30 02:30:47 +0800

    Update helper_log.py

--- app/helper_log.py
+++ app/helper_log.py
@@ -6,7 +6,7 @@ import streamlit as st
 SPREADSHEET_LOG = st.secrets["gsheet"]["log_wlc_dev_id"]
 
 
-def save_log_dev(activity, desc, status):
+def log_dev(activity, desc, status):
     try:
         sheet = open_worksheet(SPREADSHEET_LOG, "log_dev")
         now = pd.Timestamp.now(tz='Asia/Kuala_Lumpur').strftime('%Y-%m-%d %H:%M:%S')
