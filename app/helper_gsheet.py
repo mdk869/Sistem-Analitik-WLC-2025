95b04d27a73c403698bc149b56112f3f5d6604ee
mdk869 <muhammad.khalis.rahim@gmail.com>
2025-06-30 00:07:07 +0800

    update utils dan get_worksheet

--- app/helper_gsheet.py
+++ app/helper_gsheet.py
@@ -1,21 +1,8 @@
 import pandas as pd
 import streamlit as st
 from app.helper_log import log_error
+from app.helper_utils import get_worksheet
 
-
-# ✅ Fungsi dapatkan worksheet, cipta jika tidak wujud
-def get_worksheet(spreadsheet, worksheet_name, create_if_not_exist=True):
-    try:
-        ws = spreadsheet.worksheet(worksheet_name)
-    except Exception:
-        if create_if_not_exist:
-            ws = spreadsheet.add_worksheet(title=worksheet_name, rows="1000", cols="20")
-            st.info(f"✅ Worksheet '{worksheet_name}' telah dicipta.")
-        else:
-            st.error(f"❌ Worksheet '{worksheet_name}' tidak wujud.")
-            log_error(f"Worksheet '{worksheet_name}' tidak wujud.")
-            return None
-    return ws
 
 
 # ✅ Fungsi load worksheet kepada DataFrame
--- app/helper_utils.py
+++ app/helper_utils.py
@@ -248,3 +248,16 @@ def carian_nama_suggestion(df, label="Cari Nama", key=""):
     else:
         return None
 
+# ✅ Fungsi dapatkan worksheet, cipta jika tidak wujud
+def get_worksheet(spreadsheet, worksheet_name, create_if_not_exist=True):
+    try:
+        ws = spreadsheet.worksheet(worksheet_name)
+    except Exception:
+        if create_if_not_exist:
+            ws = spreadsheet.add_worksheet(title=worksheet_name, rows="1000", cols="20")
+            st.info(f"✅ Worksheet '{worksheet_name}' telah dicipta.")
+        else:
+            st.error(f"❌ Worksheet '{worksheet_name}' tidak wujud.")
+            log_error(f"Worksheet '{worksheet_name}' tidak wujud.")
+            return None
+    return ws
