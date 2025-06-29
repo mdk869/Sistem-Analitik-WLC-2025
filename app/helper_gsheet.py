3f340ca7fbc71145e8a67554a1645989e48cc622
mdk869 <muhammad.khalis.rahim@gmail.com>
2025-06-30 01:11:20 +0800

    revert

--- app/helper_gsheet.py
+++ app/helper_gsheet.py
@@ -1,6 +1,7 @@
 # app/helper_gsheet.py
 
 import streamlit as st
+<<<<<<< HEAD
 import pandas as pd
 from app.helper_connection import gc
 from app.helper_log import log_error
@@ -9,6 +10,11 @@ from app.helper_log import log_error
 # ====================================================
 # ✅ Dapatkan Worksheet (Auto Create Jika Tiada)
 # ====================================================
+=======
+
+
+
+>>>>>>> parent of 0e0cbee (kemaskini log fail)
 def get_worksheet(spreadsheet, worksheet_name):
     try:
         ws = spreadsheet.worksheet(worksheet_name)
@@ -17,9 +23,12 @@ def get_worksheet(spreadsheet, worksheet_name):
     return ws
 
 
+<<<<<<< HEAD
 # ====================================================
 # ✅ Load Worksheet ke DataFrame
 # ====================================================
+=======
+>>>>>>> parent of 0e0cbee (kemaskini log fail)
 def load_worksheet_to_df(spreadsheet, worksheet_name):
     try:
         ws = get_worksheet(spreadsheet, worksheet_name)
@@ -27,6 +36,7 @@ def load_worksheet_to_df(spreadsheet, worksheet_name):
         df = pd.DataFrame(data)
         return df
     except Exception as e:
+<<<<<<< HEAD
         st.error(f"❌ Gagal load data {worksheet_name}: {e}")
         log_error(f"load_worksheet_to_df error {worksheet_name} - {e}")
         return pd.DataFrame()
@@ -46,3 +56,8 @@ def save_df_to_worksheet(spreadsheet, worksheet_name, df: pd.DataFrame):
         st.error(f"❌ Gagal simpan data ke {worksheet_name}: {e}")
         log_error(f"save_df_to_worksheet error {worksheet_name} - {e}")
         return False
+=======
+        st.error(f"Gagal load data {worksheet_name}: {e}")
+        log_error(f"load_worksheet_to_df error {worksheet_name} - {e}")
+        return pd.DataFrame()
+>>>>>>> parent of 0e0cbee (kemaskini log fail)
