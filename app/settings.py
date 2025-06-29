845a8373c1f6b4d8e136dc5336e4971b9fab40f8
mdk869 <muhammad.khalis.rahim@gmail.com>
2025-06-29 20:51:57 +0800

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -5,3 +5,12 @@ TARIKH_TIMBANG_SET = "20 Julai 2025"
 NAMA_PROGRAM = "Weight Loss Challenge 2025"
 VERSI = "v3.2.5"
 LAST_UPDATE = "2025-06-26"
+
+from datetime import datetime
+
+info_program = {
+    "nama_program": "Weight Loss Challenge (WLC) 2025",
+    "tarikh_mula": datetime(2025, 6, 18),
+    "tarikh_tamat": datetime(2025, 8, 20),
+    "versi": "v3.3.0"
+}
