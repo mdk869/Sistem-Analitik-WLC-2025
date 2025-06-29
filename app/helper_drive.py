5dc41749dea7f4f0e642cdbbfab85f675227dbcb
mdk869 <muhammad.khalis.rahim@gmail.com>
2025-06-29 09:25:55 +0800

    Update helper_drive.py

--- app/helper_drive.py
+++ app/helper_drive.py
@@ -3,7 +3,7 @@ from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
 import io
-from app.helper_log import log_error
+
 
 
 # =====================================
